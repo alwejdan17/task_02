98e202189a7beb04858d800b07dd087f7b156fd1
alwejdan17 <wejdanalharby.com>
2019-07-22 18:36:21 +0300

    task_02_Wejdan

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def msg_1(request):
+
+	context={
+	'msg':"Hello World!"
+	}
+	return render(request,'hello_world.html',context)
